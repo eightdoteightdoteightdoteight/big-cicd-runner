cc6f3f307a4d48666ce033dd0d35a37bf4d11f23
Brice REDON <bricered@protonmail.com>
2024-01-04 22:40:25 +0100

    add proto web server

---
+++ main.go
@@ -0,0 +1,32 @@
+// main.go
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", RootHandler)
+	mux.HandleFunc("hello-world", HwHandler)
+
+	var port int = 8080
+
+	fmt.Printf("Server is running on port :%d...\n", port)
+
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), mux)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+}
+
+func RootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the home page!")
+}
+
+// HelloHandler handles requests to the "/hello" endpoint
+func HwHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello world!")
+}
