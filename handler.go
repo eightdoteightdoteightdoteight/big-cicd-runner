6a922dc19005f4a834b22134a57a876288941f52
Brice REDON <bricered@protonmail.com>
2024-01-30 23:46:09 +0100

    Alice et Bob

--- handler.go
+++ handler.go
@@ -11,9 +11,9 @@ import (
 )
 
 type CiCdRequestBody struct {
-	ID       string `json:"id"`
+	Id       string `json:"id"`
 	RepoName string `json:"repository"`
-	CommitID string `json:"ref"`
+	Ref      string `json:"Ref"`
 }
 
 type CdRequestBody struct {
@@ -33,30 +33,74 @@ func CiCdHandler(w http.ResponseWriter, r *http.Request) {
 
 		var requestData CiCdRequestBody
 
+		err = json.Unmarshal(body, &requestData)
+
+		if err != nil {
+			http.Error(w, "Erreur lors du décodage du JSON", http.StatusBadRequest)
+			return
+		}
+
+		id := requestData.Id
+		repoName := requestData.RepoName
+		ref := requestData.Ref
+
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("Requête POST traitée avec succès"))
 
-		repoURL := "https://github.com/" + requestData.RepoName + ".git"
+		repoURL := "https://github.com/" + repoName + ".git"
+		projectName := strings.Split(repoName, "/")[1]
+		tag := strings.Split(ref, "heads/")[1]
 
-		if exists, err := folderExists(requestData.RepoName); err != nil {
+		cd_home := exec.Command("cd", "~")
+		cd_home_output, err := cd_home.CombinedOutput()
+		if err != nil {
+			fmt.Println("Erreur lors de l'exécution de la commande:", string(cd_home_output))
+		}
+
+		if exists, err := folderExists(repoName); err != nil {
 			fmt.Println("Error:", err)
 			return
-		} else if exists {
-			cmd := exec.Command("git", "-C", requestData.RepoName, "fetch", "--all")
-			output, err := cmd.CombinedOutput()
-			if err != nil {
-				fmt.Println("Erreur lors de l'exécution de la commande:", string(output))
-			}
-		} else {
+		} else if exists == false {
 			cmd := exec.Command("git", "clone", repoURL)
 			output, err := cmd.CombinedOutput()
 			if err != nil {
 				fmt.Println("Erreur lors de l'exécution de la commande:", string(output))
 			}
 		}
+		checkout := fmt.Sprintf("origin/%s", ref)
+		cmd := exec.Command("cd", repoName, ";", "git", "checkout", checkout, ";", "git", "pull")
+		output, err := cmd.CombinedOutput()
+		if err != nil {
+			fmt.Println("Erreur lors de l'exécution de la commande:", string(output))
+		}
 
-		pathToYaml := requestData.RepoName + "/big_ci.yml"
-		stagesExecution(pathToYaml, requestData.ID)
+		pathToYaml := "/big_ci.yml"
+		stagesExecution(pathToYaml, id)
+
+		go func() {
+			oldImage, err := updateDeployment("imt-framework-staging", projectName, tag)
+			if err != nil {
+				fmt.Println("Error:", err)
+				http.Error(w, "Erreur lors du déploiement", http.StatusInternalServerError)
+				return
+			}
+			sendJobResult(id, "Déploiement sur Kubernetes", "Déploiement terminé sur imt-framework-staging", "Success")
+			time.Sleep(10 * time.Second)
+
+			if health := isHealthy("imt-framework-staging", projectName); health {
+				sendJobResult(id, "Tests de santé", "Les tests de santé ont été réussis", "Success")
+			} else {
+				sendJobResult(id, "Tests de santé", "Les tests de santé ont échoué", "Failed")
+				oldImageSplit := strings.Split(oldImage, ":")
+				if _, err := updateDeployment("imt-framework-staging", oldImageSplit[0], oldImageSplit[1]); err != nil {
+					fmt.Println("Error:", err)
+					http.Error(w, "Erreur lors du rollback", http.StatusInternalServerError)
+					return
+				}
+				sendJobResult(id, "Rollback", fmt.Sprintf("Rollback effectué à la version %s", oldImageSplit[1]), "Success")
+			}
+
+			finishPipeline(id)
+		}()
 
 	} else {
 		fmt.Println("Méthode non autorisée")
